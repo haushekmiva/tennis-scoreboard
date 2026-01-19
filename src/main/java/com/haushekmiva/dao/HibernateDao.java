f1bc8f59ff7f9d5a8a1c44ac2870308b56b6ae22
haushekmiva <chuminitoGordito@proton.me>
2026-01-19 20:52:22 +0300

    refactor: add DAO layer abstraction instead of concrete implementations

--- src/main/java/com/haushekmiva/dao/HibernateDao.java
+++ src/main/java/com/haushekmiva/dao/HibernateDao.java
@@ -1,19 +1,29 @@
 package com.haushekmiva.dao;
 
 import com.haushekmiva.exceptions.DataAccessException;
-import com.haushekmiva.model.Player;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import java.io.Serializable;
 
 import static com.haushekmiva.HibernateUtil.getSessionFactory;
 
-public class HibernatePlayerDao {
+public abstract class HibernateDao<T, ID extends Serializable> {
 
-    public void savePlayer(Player player) {
+    private final Class<T> entityClass;
+    protected SessionFactory sessionFactory;
+
+    public HibernateDao(Class<T> entityClass, SessionFactory sessionFactory) {
+        this.entityClass = entityClass;
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void savePlayer(T entity) {
         Transaction transaction = null;
         try (Session session = getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            session.persist(player);
+            session.persist(entity);
             transaction.commit();
         } catch (Exception e) { // TODO: сузить исключения и создать более конкретные
             if (transaction != null) {
