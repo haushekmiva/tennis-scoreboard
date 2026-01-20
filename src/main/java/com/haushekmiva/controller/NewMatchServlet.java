6abfbb101eb5de98600ee1ed8fd0bd32c1ec8a8d
haushekmiva <chuminitoGordito@proton.me>
2026-01-20 22:17:32 +0300

    feat: add NewMatchServlet and doGet method

---
+++ src/main/java/com/haushekmiva/controller/NewMatchServlet.java
@@ -0,0 +1,23 @@
+package com.haushekmiva.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/new-match")
+public class NewMatchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/new-match.jsp");
+        dispatcher.forward(request, response);
+    }
+
+}
