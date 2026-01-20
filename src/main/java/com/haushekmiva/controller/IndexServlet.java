f9877ba88f2858e935ec38a0a0663489810d06ff
haushekmiva <chuminitoGordito@proton.me>
2026-01-20 21:56:42 +0300

    feat: update JSP page and add JSP libraries

--- src/main/java/com/haushekmiva/controller/IndexServlet.java
+++ src/main/java/com/haushekmiva/controller/IndexServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/index")
+@WebServlet("")
 public class IndexServlet extends HttpServlet {
 
     @Override
