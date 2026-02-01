98e0b86806bb5025984a2fec9bc74996ee871f32
haushekmiva <chuminitoGordito@proton.me>
2026-02-01 20:43:25 +0300

    feat: add ApplicationException class

---
+++ src/main/java/com/haushekmiva/exceptions/ApplicationException.java
@@ -0,0 +1,15 @@
+package com.haushekmiva.exceptions;
+
+public class ApplicationException extends RuntimeException {
+    public ApplicationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
