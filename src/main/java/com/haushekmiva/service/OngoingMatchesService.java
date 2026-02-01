81594d1205d97e33b99745b062680249cf26d4ec
haushekmiva <chuminitoGordito@proton.me>
2026-02-01 20:52:15 +0300

    refactor: change getMatch to throw exception instead of returning Optional

--- src/main/java/com/haushekmiva/service/OngoingMatchesService.java
+++ src/main/java/com/haushekmiva/service/OngoingMatchesService.java
@@ -2,8 +2,10 @@ package com.haushekmiva.service;
 
 import com.haushekmiva.dto.MatchInformation;
 import com.haushekmiva.dto.MatchParticipantIds;
+import com.haushekmiva.exceptions.ResourceNotFoundException;
 import com.haushekmiva.model.OngoingMatchScore;
 
+import java.nio.file.ReadOnlyFileSystemException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,8 +15,13 @@ public class OngoingMatchesService {
 
     private final Map<UUID, OngoingMatchScore> matches = new ConcurrentHashMap<>();
 
-    public Optional<OngoingMatchScore> getMatch(UUID matchId) {
-        return Optional.ofNullable(matches.get(matchId));
+    public OngoingMatchScore getMatch(UUID matchId) {
+        OngoingMatchScore match = matches.get(matchId);
+        if (match != null) {
+            return match;
+        }
+
+        throw new ResourceNotFoundException(String.format("Match with UUID %s not found.", matchId));
     }
 
     public UUID createNewMatch(MatchInformation matchInformation) {
