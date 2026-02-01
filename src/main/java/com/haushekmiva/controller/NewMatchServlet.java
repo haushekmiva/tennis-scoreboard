981ca63dcf2f5fcf38075fa1c24abfeccfae1063
haushekmiva <chuminitoGordito@proton.me>
2026-02-01 18:03:27 +0300

    refactor: complete NewMatchServlet methods

--- src/main/java/com/haushekmiva/controller/NewMatchServlet.java
+++ src/main/java/com/haushekmiva/controller/NewMatchServlet.java
@@ -1,13 +1,25 @@
 package com.haushekmiva.controller;
 
+import com.haushekmiva.dao.HibernateDao;
+import com.haushekmiva.dao.PlayerHibernateDao;
+import com.haushekmiva.dto.MatchInformation;
+import com.haushekmiva.dto.MatchParticipantIds;
+import com.haushekmiva.model.OngoingMatchScore;
+import com.haushekmiva.model.Player;
+import com.haushekmiva.service.OngoingMatchesService;
+import com.haushekmiva.service.PlayerCheckService;
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.SessionFactory;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
 
 @WebServlet("/new-match")
 public class NewMatchServlet extends HttpServlet {
@@ -21,13 +33,32 @@ public class NewMatchServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        ServletContext context = request.getServletContext();
         String firstPlayerName = request.getParameter("firstPlayerName");
         String secondPlayerName = request.getParameter("secondPlayerName");
 
-        System.out.println(firstPlayerName + " " + secondPlayerName);
+        // добавить валидацию
 
+        // тут у нас PlayerCheckService
+        PlayerCheckService playerCheckService = (PlayerCheckService) context.getAttribute("playerCheckService");
+        MatchParticipantIds matchParticipantIds = playerCheckService.getPlayerIds(firstPlayerName, secondPlayerName);
+
+        Long firstPlayerId = matchParticipantIds.firstPlayerId();
+        Long secondPlayerId = matchParticipantIds.secondPlayerId();
+
+        OngoingMatchesService ongoingMatchesService = (OngoingMatchesService) context.getAttribute("ongoingMatchesService");
+        UUID matchUUID = ongoingMatchesService.createNewMatch(
+                new MatchInformation(
+                        firstPlayerId,
+                        firstPlayerName,
+                        secondPlayerId,
+                        secondPlayerName
+                )
+        );
+
+        response.sendRedirect(request.getContextPath() + "/match-score?uuid=" + matchUUID.toString());
     }
 
 }
