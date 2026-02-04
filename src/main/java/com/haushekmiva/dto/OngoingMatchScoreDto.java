c300ba9493e3056cc1fee67ad5dee6636cc8fe3b
haushekmiva <chuminitoGordito@proton.me>
2026-02-04 17:07:26 +0300

    chore: add fields for player ids

--- src/main/java/com/haushekmiva/dto/OngoingMatchScoreDto.java
+++ src/main/java/com/haushekmiva/dto/OngoingMatchScoreDto.java
@@ -1,10 +1,12 @@
 package com.haushekmiva.dto;
 
 public record OngoingMatchScoreDto(
+        String firstPlayerId,
         String firstPlayerName,
         String firstPlayerPoints,
         String firstPlayerGames,
         String firstPlayerSets,
+        String secondPlayerId,
         String secondPlayerName,
         String secondPlayerPoints,
         String secondPlayerGames,
