bb41ea13109be1dd8d8e97420a177f8cfcd4ced8
haushekmiva <chuminitoGordito@proton.me>
2026-02-05 19:14:15 +0300

    feat: add post method handler in MatchScoreServlet

--- src/main/java/com/haushekmiva/controller/MatchScoreServlet.java
+++ src/main/java/com/haushekmiva/controller/MatchScoreServlet.java
@@ -1,11 +1,14 @@
 package com.haushekmiva.controller;
 
+import com.haushekmiva.dto.FinishedMatchDto;
 import com.haushekmiva.dto.OngoingMatchScoreDto;
 import com.haushekmiva.exceptions.InvalidParameterValueException;
+import com.haushekmiva.mapper.FinishedMatchMapper;
 import com.haushekmiva.mapper.MatchMapper;
 import com.haushekmiva.model.OngoingMatchScore;
+import com.haushekmiva.service.FinishedMatchesPersistenceService;
+import com.haushekmiva.service.MatchScoreCalculationService;
 import com.haushekmiva.service.OngoingMatchesService;
-import com.haushekmiva.validation.ValidationErrorMessages;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
@@ -17,7 +20,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
-import static com.haushekmiva.validation.InputValidation.checkFieldEmpty;
 import static com.haushekmiva.validation.RequestValidation.checkRequestParameterEmpty;
 
 @WebServlet("/match-score")
@@ -28,22 +30,10 @@ public class MatchScoreServlet extends HttpServlet {
         ServletContext context = request.getServletContext();
         OngoingMatchesService ongoingMatchesService = (OngoingMatchesService) context.getAttribute("ongoingMatchesService");
 
-        String matchUUIDRaw = request.getParameter("uuid");
-        checkRequestParameterEmpty(matchUUIDRaw, "uuid");
+        UUID matchUuid = extractUuid(request);
+        OngoingMatchScore ongoingMatchScore = ongoingMatchesService.getMatch(matchUuid);
 
-        // надо будет как-нибудь вынести
-        UUID matchUUID;
-        try {
-            matchUUID = UUID.fromString(matchUUIDRaw);
-        } catch (IllegalArgumentException e) {
-            throw new InvalidParameterValueException("Invalid UUID parameter format.");
-        }
-
-        OngoingMatchScore ongoingMatchScore = ongoingMatchesService.getMatch(matchUUID);
-
-        // хз работает ли, надо будет проверить
         OngoingMatchScoreDto ongoingMatchScoreDto = MatchMapper.INSTANCE.ongoingMatchScoreToDto(ongoingMatchScore);
-
         request.setAttribute("ongoingMatchScoreDto", ongoingMatchScoreDto);
 
         response.setCharacterEncoding("UTF-8");
@@ -51,4 +41,53 @@ public class MatchScoreServlet extends HttpServlet {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/match-score.jsp");
         dispatcher.forward(request, response);
     }
+
+    // post method
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext context = request.getServletContext();
+        OngoingMatchesService ongoingMatchesService = (OngoingMatchesService) context.getAttribute(
+                "ongoingMatchesService");
+        MatchScoreCalculationService matchScoreCalculationService = (MatchScoreCalculationService) context.getAttribute(
+                "matchScoreCalculationService");
+        FinishedMatchesPersistenceService finishedMatchesPersistenceService =
+                (FinishedMatchesPersistenceService) context.getAttribute("finishedMatchesPersistenceService");
+
+        UUID matchUuid = extractUuid(request);
+        Long playerId = Long.valueOf(request.getParameter("playerId")); // добавить валидацию
+        OngoingMatchScore ongoingMatchScore = ongoingMatchesService.getMatch(matchUuid);
+        matchScoreCalculationService.doMove(ongoingMatchScore, playerId);
+
+        if (!ongoingMatchScore.isMatchFinished()) {
+            OngoingMatchScoreDto ongoingMatchScoreDto = MatchMapper.INSTANCE.ongoingMatchScoreToDto(ongoingMatchScore);
+            request.setAttribute("ongoingMatchScoreDto", ongoingMatchScoreDto);
+        } else {
+            ongoingMatchesService.removeMatch(matchUuid);
+
+            finishedMatchesPersistenceService.saveFinishedMatch(ongoingMatchScore);
+            FinishedMatchDto finishedMatchDto = FinishedMatchMapper.INSTANCE.ongoingMatchScoreToFinishedMatchDto(ongoingMatchScore);
+            request.setAttribute("finishedMatchDto", finishedMatchDto);
+
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html; charset=UTF-8");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/finished-match-score.jsp");
+            dispatcher.forward(request, response);
+            return;
+        }
+
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/match-score.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private UUID extractUuid(HttpServletRequest request) {
+        String matchUUIDRaw = request.getParameter("uuid");
+        checkRequestParameterEmpty(matchUUIDRaw, "uuid");
+        try {
+            return UUID.fromString(matchUUIDRaw);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidParameterValueException("Invalid UUID parameter format.");
+        }
+    }
+
 }
