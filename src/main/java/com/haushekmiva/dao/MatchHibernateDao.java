a641653fcefe944300456dae75c3e83c1d1044be
haushekmiva <chuminitoGordito@proton.me>
2026-02-07 13:17:10 +0300

    chore: add methods to get total count and paginated subset in MatchHibernateDao

--- src/main/java/com/haushekmiva/dao/MatchHibernateDao.java
+++ src/main/java/com/haushekmiva/dao/MatchHibernateDao.java
@@ -1,12 +1,38 @@
 package com.haushekmiva.dao;
 
+import com.haushekmiva.exceptions.DataAccessException;
 import com.haushekmiva.model.Match;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import java.util.List;
 
 public class MatchHibernateDao extends HibernateDao<Match, Long> {
 
     public MatchHibernateDao(SessionFactory sessionFactory) {
         super(Match.class, sessionFactory);
+    }
+
+    public Long getMatchCount() {
+        try (Session session = super.getSessionFactory().openSession()) {
+            return session.createQuery("select count(e) from Match e", Long.class)
+                    .uniqueResult();
+        } catch (HibernateException e) {
+            throw new DataAccessException("Ошибка при получении кол-ва записей из бд.", e);
+        }
+    }
+
+    public List<Match> fetchMatchesSubset(int from, int to) {
+        try (Session session = super.getSessionFactory().openSession()) {
+            return session.createQuery("from Match", Match.class)
+                    .setFirstResult(from)
+                    .setMaxResults(to)
+                    .list();
+        }
+        catch (HibernateException e) {
+            throw new DataAccessException("Ошибка при получении данных из бд.");
+        }
     }
 
 }
