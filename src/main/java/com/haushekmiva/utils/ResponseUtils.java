660c21fb568d9f46d91848cccb6394c37f98af24
haushekmiva <chuminitoGordito@proton.me>
2026-02-09 21:31:09 +0300

    chore(ResponseUtils): add userRedirect method to simplify user redirect with arguments

--- src/main/java/com/haushekmiva/utils/ResponseUtils.java
+++ src/main/java/com/haushekmiva/utils/ResponseUtils.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class ResponseUtils {
 
@@ -16,6 +18,23 @@ public final class ResponseUtils {
         response.setContentType("text/html; charset=UTF-8");
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/" + forwardPageName);
         dispatcher.forward(request, response);
+    }
+
+    public static void redirectUser(HttpServletResponse response, String endpoint, Map<String, String> parameters) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(endpoint);
+        if (!parameters.isEmpty()) {
+            boolean isFirstElement = true;
+            for (Map.Entry<String, String> parameter : parameters.entrySet()) {
+                if (isFirstElement) {
+                    stringBuilder.append("?");
+                    isFirstElement = false;
+                } else stringBuilder.append("&");
+                stringBuilder.append(parameter.getKey()).append("=").append(parameter.getValue());
+            }
+        }
+        response.sendRedirect(stringBuilder.toString());
+
     }
 
 }
