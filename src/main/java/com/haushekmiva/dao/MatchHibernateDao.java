f438eaa835267e1f5597d617573056d48999bc51
haushekmiva <chuminitoGordito@proton.me>
2026-02-09 16:42:41 +0300

    chore(MatchHibernateDao): add functions to get matches subset and matches count by player name from db

--- src/main/java/com/haushekmiva/dao/MatchHibernateDao.java
+++ src/main/java/com/haushekmiva/dao/MatchHibernateDao.java
@@ -7,6 +7,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import java.util.List;
+import java.util.Objects;
+
+import static org.hibernate.Hibernate.list;
 
 public class MatchHibernateDao extends HibernateDao<Match, Long> {
 
@@ -23,9 +26,32 @@ public class MatchHibernateDao extends HibernateDao<Match, Long> {
         }
     }
 
+    public Long getMatchCountByPlayerName(String playerName) {
+        try (Session session = super.getSessionFactory().openSession()) {
+            return session.createQuery("select count(e) from Match e where e.winner.name like :name", Long.class)
+                    .setParameter("name", "%" + playerName + "%")
+                    .uniqueResult();
+        } catch (HibernateException e) {
+            throw new DataAccessException("Ошибка при получении кол-ва записей из бд.", e);
+        }
+    }
+
     public List<Match> fetchMatchesSubset(int offset, int count) {
         try (Session session = super.getSessionFactory().openSession()) {
             return session.createQuery("from Match", Match.class)
+                    .setFirstResult(offset)
+                    .setMaxResults(count)
+                    .list();
+        }
+        catch (HibernateException e) {
+            throw new DataAccessException("Ошибка при получении данных из бд.");
+        }
+    }
+
+    public List<Match> fetchMatchesSubsetByPlayerName(int offset, int count, String playerName) {
+        try (Session session = super.getSessionFactory().openSession()) {
+            return session.createQuery("from Match e where e.winner.name like :name", Match.class)
+                    .setParameter("name", "%" + playerName + "%")
                     .setFirstResult(offset)
                     .setMaxResults(count)
                     .list();
