ea5270f6393a32ecd0e7d24feb10b8ac2e069834
haushekmiva <chuminitoGordito@proton.me>
2026-02-13 23:29:17 +0300

    refactor(PlayerCheckService): optimize imports

--- src/main/java/com/haushekmiva/service/PlayerCheckService.java
+++ src/main/java/com/haushekmiva/service/PlayerCheckService.java
@@ -1,9 +1,6 @@
 package com.haushekmiva.service;
 
 import com.haushekmiva.dto.MatchParticipantIds;
-import com.haushekmiva.model.Player;
-
-import java.util.Optional;
 
 public interface PlayerCheckService {
     MatchParticipantIds getPlayerIds(String firstPlayerName, String secondPlayerName);
